8c974d504ce29cc5d2d19b5cbcb5531e11833835
Chamsol Kim <kcsol1005@gmail.com>
2024-07-28 11:29:32 +0900

    set a tile color of currently playing track, set an opacity to the play button of the unstreamable track

--- lib/feature/album/views/widgets/track_list_item.dart
+++ lib/feature/album/views/widgets/track_list_item.dart
@@ -38,11 +38,11 @@ class _TrackListItemState extends State<TrackListItem> {
       _animationController?.reset();
     }
 
-    final titleText = "${widget.track.trackName} ";
+    final titleText = "${widget.track.trackName}      ";
     final titleTextStyle = TextStyle(
       color: Colors.grey.shade100,
       fontSize: 16,
-      fontWeight: FontWeight.w500,
+      fontWeight: widget.playing ? FontWeight.bold : FontWeight.w500,
     );
     final titleTextPainter = TextPainter(
       text: TextSpan(text: titleText, style: titleTextStyle),
@@ -51,51 +51,61 @@ class _TrackListItemState extends State<TrackListItem> {
     )..layout();
     final titleWidth = titleTextPainter.size.width;
 
-    return ListTile(
-      titleTextStyle: titleTextStyle,
-      leading: Transform.scale(
-        scale: 0.8,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          clipBehavior: Clip.hardEdge,
-          child: Image.network(widget.track.artworkUrl60),
-        ),
-      ),
-      title: LayoutBuilder(
-        builder: (context, constraints) {
-          final overflows = constraints.maxWidth < titleWidth;
-          final text = titleText + (overflows ? titleText : "");
-
-          return ClipRect(
+    return Container(
+      color: widget.playing ? Colors.black12 : null,
+      child: ListTile(
+        titleTextStyle: titleTextStyle,
+        leading: Transform.scale(
+          scale: 0.8,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+            ),
             clipBehavior: Clip.hardEdge,
-            child: Text(
-              text,
-              style: titleTextStyle,
-              maxLines: 1,
-              softWrap: false,
-              overflow: TextOverflow.visible,
-            )
-                .animate(
-                  autoPlay: false,
-                  target: overflows && widget.playing ? 1 : 0,
-                  onInit: _onAnimationInit,
-                  onComplete: _onAnimationComplete,
-                )
-                .moveX(
-                  begin: 0,
-                  end: -titleWidth,
-                  duration: 10.seconds,
-                ),
-          );
-        },
-      ),
-      trailing: IconButton(
-        onPressed: () => widget.onPlayPressed(widget.track),
-        icon: Icon(
-          Icons.play_circle_fill_rounded,
-          color: Colors.grey.shade100,
+            child: Image.network(widget.track.artworkUrl60),
+          ),
+        ),
+        title: LayoutBuilder(
+          builder: (context, constraints) {
+            final overflows = constraints.maxWidth < titleWidth;
+            final text = titleText + (overflows ? titleText : "");
+
+            return ClipRect(
+              clipBehavior: Clip.hardEdge,
+              child: Text(
+                text,
+                style: titleTextStyle,
+                maxLines: 1,
+                softWrap: false,
+                overflow: TextOverflow.visible,
+              )
+                  .animate(
+                    autoPlay: false,
+                    target: overflows && widget.playing ? 1 : 0,
+                    onInit: _onAnimationInit,
+                    onComplete: _onAnimationComplete,
+                  )
+                  .moveX(
+                    begin: 0,
+                    end: -titleWidth,
+                    duration: 10.seconds,
+                  ),
+            );
+          },
+        ),
+        trailing: Opacity(
+          opacity: widget.track.isStreamable ? 1 : 0.5,
+          child: IconButton(
+            onPressed: widget.track.isStreamable
+                ? () => widget.onPlayPressed(widget.track)
+                : null,
+            icon: Icon(
+              widget.playing
+                  ? Icons.pause_circle_filled_rounded
+                  : Icons.play_circle_fill_rounded,
+              color: Colors.grey.shade100,
+            ),
+          ),
         ),
       ),
     );
